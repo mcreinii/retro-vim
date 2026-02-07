62512d4d4870c17b8300fabf69f5a46bf4a68a10
mcreinii <mcreinii@gmail.com>
2026-02-07 16:22:50 +0800

    Added Neogit.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -4,4 +4,19 @@ return {
         'lewis6991/gitsigns.nvim',
         opts = {},
     },
+
+    -- NeoGit
+    {
+        'NeogitOrg/neogit',
+        lazy = true,
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'sindrets/diffview.nvim',
+            'ibhagwan/fzf-lua',
+        },
+        cmd = 'Neogit',
+        keys = {
+            { '<leader>g', '<cmd>Neogit<cr>', desc = 'Open Neogit' },
+        },
+    },
 }
