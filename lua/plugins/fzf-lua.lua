70f0242a10494f023fa92878cf8fb2bd36e5b6f1
mcreinii <mcreinii@gmail.com>
2026-02-10 11:02:13 +0800

    Single Border

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,10 +1,10 @@
+local get_border = require('utils.ui').get_border()
 require('fzf-lua').setup({
-
 	fzf_opts = { ['--layout'] = 'reverse' },
 	winopts = {
 		height = 0.85,
 		width = 0.85,
-		preview = { hidden = 'nohidden', vertical = 'down:45%', title = ' Preview ' },
-		border = vim.o.winborder or 'single',
+		preview = { hidden = 'nohidden', vertical = 'down:45%', title = ' Preview ', border = get_border },
+		border = get_border,
 	},
 })
