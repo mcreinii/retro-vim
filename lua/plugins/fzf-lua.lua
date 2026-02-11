b8bee8aede03a04298a0d599641af3ca8af615a2
mcreinii <mcreinii@gmail.com>
2026-02-11 21:14:37 +0800

    DROPPING VIMPLUG

--- lua/plugins/fzf-lua.lua
+++ lua/plugins/fzf-lua.lua
@@ -1,10 +1,9 @@
-local get_border = require('utils.ui').get_border()
 require('fzf-lua').setup({
 	fzf_opts = { ['--layout'] = 'reverse' },
 	winopts = {
 		height = 0.85,
 		width = 0.85,
 		preview = { hidden = 'nohidden', vertical = 'down:45%', title = ' Preview ', border = get_border },
-		border = get_border,
+		border = 'single',
 	},
 })
